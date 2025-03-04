5d09b8ecd3b122e616edf47a0c8d3857e01efa59
Angel Yahir Poot Ucan <135137591+YahirPoot@users.noreply.github.com>
2025-03-04 00:20:19 -0600

    init project maps

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { FullscreenMapPageComponent, HousesPageComponent, MarkersPageComponent } from './pages';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {
+    path: 'fullscreen',
+    component: FullscreenMapPageComponent,
+    title: 'Fullscreen Map',
+  },
+  {
+    path: 'markers',
+    component: MarkersPageComponent,
+    title: 'Marcadores',
+  },
+  {
+    path: 'houses',
+    component: HousesPageComponent,
+    title: 'Propiedades disponibles',
+  },
+  {
+    path: '**',
+    redirectTo: 'fullscreen',
+  },
+
+];
---
+++ src/app/pages/index.ts
@@ -0,0 +1,3 @@
+export * from './fullscreen-map-page/fullscreen-map-page.component';
+export * from './houses-page/houses-page.component';
+export * from './markers-page/markers-page.component';
