7879266e5efd7d3da7b9cc52e457aac9cb76e573
Angel Yahir Poot Ucan <135137591+YahirPoot@users.noreply.github.com>
2025-03-04 12:31:05 -0600

    creación de  enviroments mendiante un script

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,8 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { routes } from '../../../app.routes';
+import { NavigationEnd, Router, RouterLink } from '@angular/router';
+import { filter, map, tap } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [ AsyncPipe, RouterLink],
   templateUrl: './navbar.component.html',
 })
-export class NavbarComponent { }
+export class NavbarComponent {
+  router = inject(Router);
+
+  routes = routes.map((route) => ({
+    path: route.path,
+    title: `${route.title ?? 'Maps en Angular'}`
+  })).filter((route) => route.path !== '**');
+
+  //* Sacando el titulo de la ruta actual como un observable
+  //* Cuando tenga el signo $ al final de una variable, significa que es un observable
+  pageTitle$ = this.router.events.pipe(
+    filter((event) => event instanceof NavigationEnd),
+    tap((event) => console.log(event)),
+    map((event) => event.url),
+    map((url) => this.routes.find((route) =>  `/${route.path}` === url)?.title ?? 'Maps en Angular')
+  );
+
+  // Sacando el titulo de la ruta actual como una señal
+  //  Por alguna razon no me reconoce el toSignal
+  // pageTitle = toSignal(
+  //   this.router.events.pipe(
+  //     filter((event) => event instanceof NavigationEnd),
+  //     tap((event) => console.log(event)),
+  //     map((event) => event.url),
+  //     map((url) => this.routes.find((route) =>  `/${route.path}` === url)?.title ?? 'Maps en Angular')
+  //   );
+  // )
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+
+  export const environment = {
+    mapboxKey: 'pk.eyJ1IjoieWFoaXJwb290MTkiLCJhIjoiY203dXN3Z3QzMDRndDJqb2RkN3h6YnBiYiJ9.H4FSrJhHe3_-MR2kUX8sVg',
+  }
+
